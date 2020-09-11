2b87720a5cafe4a54442488f4362f414ceae9770
Sanjay Mali <malisanjay5433@gmail.com>
2020-09-11 10:53:38 +0530

    Initial Commit

---
+++ ShaadiAssignment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ShaadiAssignment
+//
+//  Created by Sanjay Mali on 11/09/20.
+//  Copyright © 2020 theappmaker. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
